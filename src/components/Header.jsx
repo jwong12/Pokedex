84e048fd151cdd87a55d842ce73383e828eacdef
James Wong <jwong_li@hotmail.com>
2020-10-11 22:40:42 -0700

    Added node-sass module and added styles to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { css, cx } from 'emotion';
+import '../stylesheets/Header.scss';
 
 function Header() {
   return (
     <div>
-      <h1>Pokedex App</h1>
       <img 
         src="./images/pokemon-banner.svg" 
-        alt="Pokemon Banner"
+        alt="Pokemon banner"
         className={css`
         width: 50%;
         max-width: 550px;
+        padding: 20px 0;
       `}/><br/>
-      <input type="text" placeholder="Search"/>
+      <h1>Pokedex App</h1>
+      <div id="search-wrapper">
+        <input type="text" id="search-bar" placeholder="Search pokemon..."/>
+        <div id="button-wrapper">
+          <img src="./images/search.svg" id="search-button" alt="Search icon"/>
+        </div>
+      </div>
     </div>
   );
 }
