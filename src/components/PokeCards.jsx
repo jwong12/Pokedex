b941561076ae5e5b1e86bfdf6283292e89cbf1f9
James Wong <jwong_li@hotmail.com>
2020-10-12 22:07:42 -0700

    Abstracted fetch requests from PokeCards component to PokedexApp

--- src/components/PokeCards.jsx
+++ src/components/PokeCards.jsx
@@ -5,48 +5,13 @@ import '../stylesheets/PokeCards.scss';
 class PokeCards extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			pokeData: [],
-			cards: []
-		};
-	}
-
-	componentDidMount() {
-		this.fetchPokemonNames();
-	}
-
-	fetchPokemonNames() {
-		const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?offset=' + Math.floor(Math.random() * 500) +'&limit=10';
-		fetch(apiUrl)
-			.then((response) => response.json())
-			.then((data) => this.setState({pokeData: data.results}))
-			.then(() => {
-				const { pokeData } = this.state;
-
-				for (let item of pokeData) {
-					this.fetchPokemonDetails(item.name);
-				}
-			})
-			.catch((err) => console.log(err));
-	}
-
-	fetchPokemonDetails(name) {
-		fetch('https://pokeapi.co/api/v2/pokemon/' + name)
-			.then((response) => response.json())
-			.then((data) => {
-				const { cards } = this.state;
-				const pokemonArray = [...cards];
-				pokemonArray.push(data);
-				this.setState({ cards: pokemonArray })
-			})
-			.catch((err) => console.log(err));
 	}
 
 	render() {
 		return (
 			<div id="body-content">
 				<ul>
-					{this.state.cards.map((pokemon) => 
+					{this.props.pokeCards.map((pokemon) => 
 						<li key={pokemon.id}>
 							<PokeCard {...pokemon} />
 						</li>
--- src/components/PokedexApp.jsx
+++ src/components/PokedexApp.jsx
@@ -4,6 +4,45 @@ import { css } from 'emotion';
 import '../stylesheets/PokedexApp.scss';
 
 class PokedexApp extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			pokeData: [],
+			cards: []
+		};
+	}
+
+	componentDidMount() {
+		this.fetchPokemonNames();
+	}
+
+	fetchPokemonNames() {
+		const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?offset=' + Math.floor(Math.random() * 500) +'&limit=10';
+		fetch(apiUrl)
+			.then((response) => response.json())
+			.then((data) => this.setState({pokeData: data.results}))
+			.then(() => {
+				const { pokeData } = this.state;
+
+				for (let item of pokeData) {
+					this.fetchPokemonDetails(item.name);
+				}
+			})
+			.catch((err) => console.log(err));
+	}
+
+	fetchPokemonDetails(name) {
+		fetch('https://pokeapi.co/api/v2/pokemon/' + name)
+			.then((response) => response.json())
+			.then((data) => {
+				const { cards } = this.state;
+				const pokemonArray = [...cards];
+				pokemonArray.push(data);
+				this.setState({ cards: pokemonArray })
+			})
+			.catch((err) => console.log(err));
+	}
+
 	render() {
 		return (
 			<div>
@@ -21,7 +60,7 @@ class PokedexApp extends React.Component {
 						<img src="./images/search.svg" id="search-button" alt="Search icon"/>
 					</div>
 				</div><br/>
-				<PokeCards />
+				<PokeCards pokeCards={this.state.cards} />
 			</div>
 		);
 	}
