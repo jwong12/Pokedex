4bc19280274945f7b471b8745099835860a96c55
James Wong <jwong_li@hotmail.com>
2020-10-13 00:11:25 -0700

    Updates

---
+++ precache-manifest.bfa5400bfcf711c71a5f9532f299e575.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3cc78734e7811f361890320832cbf3",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "e518ba0692b570008abc",
+    "url": "/Pokedex/static/css/2.8f118048.chunk.css"
+  },
+  {
+    "revision": "610b05f4aa9071704091",
+    "url": "/Pokedex/static/css/main.c37a0a71.chunk.css"
+  },
+  {
+    "revision": "e518ba0692b570008abc",
+    "url": "/Pokedex/static/js/2.cc7a9156.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Pokedex/static/js/2.cc7a9156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "610b05f4aa9071704091",
+    "url": "/Pokedex/static/js/main.49f300aa.chunk.js"
+  },
+  {
+    "revision": "8529d191e13b460922b1",
+    "url": "/Pokedex/static/js/runtime-main.7a6ab017.js"
+  }
+]);
