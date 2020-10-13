ee1d58aa57734f0b71c544a121485e04dd6ed60d
James Wong <jwong_li@hotmail.com>
2020-10-13 00:06:49 -0700

    Implemented auto-complete feature for possible pokemon matches

--- src/components/PokeCards.jsx
+++ src/components/PokeCards.jsx
@@ -3,10 +3,6 @@ import PokeCard from './Card';
 import '../stylesheets/PokeCards.scss';
 
 class PokeCards extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		return (
 			<div id="body-content">
--- src/components/PokedexApp.jsx
+++ src/components/PokedexApp.jsx
@@ -6,21 +6,30 @@ import '../stylesheets/PokedexApp.scss';
 class PokedexApp extends React.Component {
 	constructor(props) {
 		super(props);
+		this.wrapperRef = React.createRef();
 		this.state = {
 			pokeData: [],
 			pokeNames: [],
 			cards: [],
+			suggestions: [],
 			text: '',
 		};
 
 		this.handleTextChange = this.handleTextChange.bind(this);
 		this.handleKeyPress = this.handleKeyPress.bind(this);
 		this.handleSearchClick = this.handleSearchClick.bind(this);
+		this.renderSuggestions = this.renderSuggestions.bind(this);
+        this.closeSuggestions = this.closeSuggestions.bind(this);
 	}
 
 	componentDidMount() {
 		this.fetchPokemonNames();
+		document.addEventListener('mousedown', this.closeSuggestions);
 	}
+
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.closeSuggestions);
+    }
 
 	fetchPokemonNames() {
 		const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=1050';
@@ -41,9 +50,9 @@ class PokedexApp extends React.Component {
 	}
 
 	fetchRandomPokemons() {
-		const { pokeNames, cards } = this.state;
+		const { pokeNames } = this.state;
 		const pokemonCards = [];
-		console.log(pokeNames)
+
 		for (let i = 0; i < 10; i++) {
 			fetch('https://pokeapi.co/api/v2/pokemon/' + pokeNames[Math.floor(Math.random() * pokeNames.length)])
 				.then((response) => response.json())
@@ -59,7 +68,6 @@ class PokedexApp extends React.Component {
 		fetch('https://pokeapi.co/api/v2/pokemon/' + name)
 			.then((response) => response.json())
 			.then((data) => {
-				const { cards } = this.state;
 				const pokemonCards = [];
 				pokemonCards.push(data);
 				this.setState({ cards: pokemonCards });
@@ -69,7 +77,17 @@ class PokedexApp extends React.Component {
 
 	handleTextChange(e) {
 		const value = e.target.value;
-		this.setState(() => ({ text: value }));
+		let suggestions = [];
+        
+        if(value.length > 0) {
+            const regex = new RegExp(`^${value}`, 'i');
+			suggestions = this.state.pokeNames.sort().filter(v => regex.test(v));
+
+			// The number of random pokemons displayed is set to 10
+            suggestions = suggestions.slice(0,10); 
+		}
+
+		this.setState(() => ({ suggestions, text: value }));
 	}
 
 	handleKeyPress(e) {
@@ -84,10 +102,38 @@ class PokedexApp extends React.Component {
 		if (text.trim() === "") {
 			this.fetchRandomPokemons();
 		} else {
-			console.log(text.toLowerCase().trim()); //
 			this.fetchPokemonDetails(text.toLowerCase().trim(), false);
 		}
 	}
+
+	suggestionSelected (value) {
+        this.setState(() => ({ 
+            text: value,
+            suggestions: []
+        }), () => this.handleSearchClick());
+	}
+
+	renderSuggestions() {
+        const { suggestions } = this.state;
+        if (suggestions.length === 0) {
+            return null;
+        }
+        return (
+            <ul>
+                {suggestions.map((item) => 
+                    <li key={item} onClick={() => this.suggestionSelected(item)}>{item}</li>
+                )}
+            </ul>
+        );
+	}
+	
+	closeSuggestions(event) {
+        if (this.wrapperRef && !this.wrapperRef.current.contains(event.target)) {
+            this.setState(() => ({ 
+                suggestions: []
+            }));
+        }
+    }
 
 	render() {
 		return (
@@ -100,22 +146,27 @@ class PokedexApp extends React.Component {
 					padding: 20px 0;
 				`}/><br/>
 				<h1>Pokedex App</h1>
-				<div id="search-wrapper">
-					<input 
-						type="text" 
-						value={this.state.text}
-						onChange={this.handleTextChange}
-						onKeyPress={this.handleKeyPress}
-						id="search-bar" 
-						placeholder="Search pokemon..."
-					/>
-					<div id="button-wrapper">
-						<img 
-							src="./images/search.svg" 
-							onClick={this.handleSearchClick}
-							id="search-button" 
-							alt="Search icon"/>
+				<div id="search-wrapper" ref={this.wrapperRef} >
+					<div id="search-container">
+						<input 
+							type="text" 
+							value={this.state.text}
+							onChange={this.handleTextChange}
+							onKeyPress={this.handleKeyPress}
+							id="search-bar" 
+							placeholder="Search pokemon..."
+							autoComplete="off"
+						/>
+						<div id="button-wrapper">
+							<img 
+								src="./images/search.svg" 
+								onClick={this.handleSearchClick}
+								id="search-button" 
+								alt="Search icon"
+							/>
+						</div>
 					</div>
+					{this.renderSuggestions()}
 				</div><br/>
 				<PokeCards pokeCards={this.state.cards} />
 			</div>
